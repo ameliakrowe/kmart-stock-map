2b0288b4f0303bfc8acf6f12903629a3426dbe6a
Amelia Rowe <ameliakathleenrowe@gmail.com>
2024-10-09 15:34:07 +1100

    search fetches data from API

--- client/src/components/ProductSearch.tsx
+++ client/src/components/ProductSearch.tsx
@@ -2,16 +2,34 @@ import { SearchBar } from "./SearchBar"
 import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
 import { useState } from 'react';
+import axios from 'axios';
+
+const requestUrl = "/api/getProductAvailability";
 
 export const ProductSearch = () => {
     const [searchInput, setSearchInput] = useState<string>("");
+
+    const fetchProductAvailability = async (productID: string) => {        
+        try {
+            const response = await axios.get(requestUrl, {
+                params: {
+                    productSKU: productID,
+                    postcode: "2000"
+                }
+            });
+            console.log(response);
+        } catch (err) {
+            console.log("error");
+        }
+    };
+
     return (
         <div className="stock-search">
             <SearchBar
                 onInput={(e: React.ChangeEvent<HTMLInputElement>) => setSearchInput(e.target.value)}
                 value={searchInput}
             />
-            <IconButton color="primary" onClick={() => console.log("clicked")}>
+            <IconButton color="primary" onClick={() => fetchProductAvailability(searchInput)}>
                 <SearchIcon />
             </IconButton>
         </div>   
