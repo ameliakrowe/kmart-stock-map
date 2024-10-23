648b6e863b3c6d80eb2e9df82d27d60ed90f23d7
Amelia Rowe <ameliakathleenrowe@gmail.com>
2024-10-23 11:55:17 +1100

    remove unused imports from slider

--- client/src/components/SearchRadius.tsx
+++ client/src/components/SearchRadius.tsx
@@ -1,13 +1,6 @@
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
 import React from 'react';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import Slider from '@mui/material/Slider';
-import MuiInput from '@mui/material/Input';
-import VolumeUp from '@mui/icons-material/VolumeUp';
 import Input from '@mui/material/Input';
 
 type SearchRadiusProps = {
